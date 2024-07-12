5bf1759fd366bf82de3b6c3947d97d5e95457c6d
Bruno Amorim <brunoamorim14@gmail.com>
2024-07-12 15:39:34 -0300

    feat: Add migration for creating products table

---
+++ database/migrations/2024_07_12_124741_create_products_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->timestamps();
+            $table->string('name');
+            $table->string('sku')->unique();
+            $table->integer('price');
+            $table->integer('stock')->default(0);
+            $table->uuid('user_id')->references('id')->on('users')->cascadeOnDelete()->cascadeOnUpdate();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
