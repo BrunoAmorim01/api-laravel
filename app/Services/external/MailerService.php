f1ad95e887228e19014b4018471fb43b73dd4d65
Bruno Amorim <brunoamorim14@gmail.com>
2024-07-11 22:46:25 -0300

    chore: Refactor MailerService to remove unused ccEmail property and constructor

--- app/Services/external/MailerService.php
+++ app/Services/external/MailerService.php
@@ -9,20 +9,12 @@ use App\Models\User;
 
 class MailerService
 {
-    private string $ccEmail;
-    public function __construct()
-    {
-        $this->ccEmail = env('MAIL_FROM_ADDRESS');
-
-    }
-
     public function sendWelcomeMail(User $user)
     {
         $mailer = new MailerSendService('Welcome to our platform! ' . $user->name);
 
 
         Mail::to($user->email)
-            //->cc($this->ccEmail)
             ->send($mailer);
     }
 }
