aa936e810282b21b392e672a6029d0900702ca81
Bruno Amorim <brunoamorim14@gmail.com>
2024-07-13 01:08:39 -0300

    feat: Add generateToken method to AuthService

--- app/Services/external/AuthService.php
+++ app/Services/external/AuthService.php
@@ -3,14 +3,14 @@
 namespace App\Services\external;
 
 use App\Models\User;
-use Illuminate\Support\Facades\Auth;
-
 
 class AuthService
 {
     public function generateToken(User $user)
     {
-
+        $token = $user->createToken('authToken')->plainTextToken;
+        return $token;
 
     }
+
 }
