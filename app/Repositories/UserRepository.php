256df1d6a00bd53e1f7474d6c8cea01f16b6749c
Bruno Amorim <brunoamorim14@gmail.com>
2024-07-13 01:07:55 -0300

    refactor: Add find method to UserRepository for retrieving user by email

--- app/Repositories/UserRepository.php
+++ app/Repositories/UserRepository.php
@@ -10,7 +10,12 @@ class UserRepository
 {
     public function create($data)
     {
-        $user= User::create($data);
+        $user = User::create($data);
         return $user;
+    }
+
+    public function find(string $email)
+    {
+        return User::where('email', $email)->first();
     }
 }
