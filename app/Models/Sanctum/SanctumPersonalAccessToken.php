f9d11351bd7e4813e502ccc086605857cf02daeb
Bruno Amorim <brunoamorim14@gmail.com>
2024-07-13 01:07:29 -0300

    feat: Add SanctumPersonalAccessToken model for Laravel Sanctum integration

---
+++ app/Models/Sanctum/SanctumPersonalAccessToken.php
@@ -0,0 +1,28 @@
+<?php
+namespace App\Models\Sanctum;
+
+use Laravel\Sanctum\PersonalAccessToken;
+
+
+class SanctumPersonalAccessToken extends PersonalAccessToken
+{
+
+
+    protected $table = 'personal_access_tokens';
+
+
+    public function tokenable()
+    {
+        return $this->morphTo('tokenable');
+    }
+
+    public static function findToken($token)
+    {
+
+        if (empty($token)) {
+            return null;
+        }
+
+        return static::where('token', hash('sha256', $token))->first();
+    }
+}
