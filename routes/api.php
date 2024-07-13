43cec653d9ca2a890113a1c54f01a7b2373b3957
Bruno Amorim <brunoamorim14@gmail.com>
2024-07-13 01:11:13 -0300

    refactor: Update authentication middleware to use Sanctum for API routes

--- routes/api.php
+++ routes/api.php
@@ -10,11 +10,13 @@ Route::group([
 ], function ($router) {
     Route::post('/register', [UsersController::class, "createUser"])->name('register');
     Route::post('/login', [UsersController::class, 'login'])->name('login');
-    Route::get('/me', [UsersController::class, 'me'])->middleware('auth:api')->name('me');
+    Route::get('/me', [UsersController::class, 'me'])
+    ->middleware('auth:sanctum')
+    ->name('me');
 });
 
 Route::prefix('products')
-    ->middleware('auth:api')
+    ->middleware('auth:sanctum')
     ->group(function () {
         Route::post('/', [ProductsController::class, "create"])->name('products.create');
     });
