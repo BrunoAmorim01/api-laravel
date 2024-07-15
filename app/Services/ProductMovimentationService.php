6597df1bd5409e720951879dfe3c3035dbbdae5a
Bruno Amorim <brunoamorim14@gmail.com>
2024-07-15 12:24:02 -0300

    feat: Add file upload functionality for proof in CreateProductMovementData

--- app/Services/ProductMovimentationService.php
+++ app/Services/ProductMovimentationService.php
@@ -5,7 +5,20 @@ namespace App\Services;
 use App\Repositories\ProductMovementData;
 use App\Repositories\ProductMovimentationDataResponse;
 use App\Repositories\ProductMovimentationRepository;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
 
+class CreateProductMovementData
+{
+    public string $productId;
+    public string $userId;
+    public int $quantity;
+    public string $type;
+    public string $reason;
+    public UploadedFile $proof;
+
+}
 
 
 class ProductMovimentationService
@@ -16,10 +29,21 @@ class ProductMovimentationService
     ) {
     }
 
-    public function create(ProductMovementData $data): ProductMovimentationDataResponse
+    public function create(CreateProductMovementData $data): ProductMovimentationDataResponse
     {
+        $filename = Str::uuid() . '.' . $data->proof->getClientOriginalExtension();
+        $result = $data->proof->storeAs('docs', $filename, 's3');
+        $fileResponse = Storage::url($result);
 
-        $productMovement = $this->productMovimentationRepository->create($data);
+        $productMovimentationToCreate = new ProductMovementData();
+        $productMovimentationToCreate->productId = $data->productId;
+        $productMovimentationToCreate->userId = $data->userId;
+        $productMovimentationToCreate->quantity = $data->quantity;
+        $productMovimentationToCreate->type = $data->type;
+        $productMovimentationToCreate->reason = $data->reason;
+        $productMovimentationToCreate->proof = $fileResponse;
+
+        $productMovement = $this->productMovimentationRepository->create($productMovimentationToCreate);
 
         return $productMovement;
 
