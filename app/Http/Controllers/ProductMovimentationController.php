2ed65e00bc8e64f43b3f03c739f6098969617072
Bruno Amorim <brunoamorim14@gmail.com>
2024-07-15 01:27:00 -0300

    feat: Add ProductMovimentationController for handling product movement requests

---
+++ app/Http/Controllers/ProductMovimentationController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\CreateProducMovimentationRequest;
+use App\Repositories\ProductMovementData;
+use App\Services\ProductMovimentationService;
+use Auth;
+
+class ProductMovimentationController extends Controller
+{
+    public function __construct(private ProductMovimentationService $productMovimentationService)
+    {
+    }
+
+    public function create(CreateProducMovimentationRequest $request)
+    {
+        $data = $request->validated();
+        $dataObj = new ProductMovementData();
+        $dataObj->productId = $data['productId'];
+        $dataObj->userId = Auth::id();
+        $dataObj->quantity = $data['quantity'];
+        $dataObj->type = $data['type'];
+        $dataObj->reason = $data['reason'];
+        //$dataObj->proof = $data['proof'];
+
+        $productMovimentation = $this->productMovimentationService->create($dataObj);
+        return response()->json($productMovimentation);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProductMovimentationController;
 use App\Http\Controllers\UsersController;
 use App\Http\Controllers\ProductsController;
 use Illuminate\Support\Facades\Route;
@@ -11,13 +12,19 @@ Route::group([
     Route::post('/register', [UsersController::class, "createUser"])->name('register');
     Route::post('/login', [UsersController::class, 'login'])->name('login');
     Route::get('/me', [UsersController::class, 'me'])
-    ->middleware('auth:sanctum')
-    ->name('me');
+        ->middleware('auth:sanctum')
+        ->name('me');
 });
 
 Route::prefix('products')
     ->middleware('auth:sanctum')
     ->group(function () {
         Route::post('/', [ProductsController::class, "create"])->name('products.create');
+    });
+
+Route::prefix('product-movimentation')
+    ->middleware('auth:sanctum')
+    ->group(function () {
+        Route::post('/', [ProductMovimentationController::class, "create"])->name('product-movimentation.create');
     });
 
