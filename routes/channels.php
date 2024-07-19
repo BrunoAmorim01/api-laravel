fba657517b3054a31c8664620a913e2d834ec770
Bruno Amorim <brunoamorim14@gmail.com>
2024-07-19 11:54:23 -0300

    Refactor broadcasting channel for everyone to use simplified syntax

--- routes/channels.php
+++ routes/channels.php
@@ -2,7 +2,12 @@
 
 use Illuminate\Support\Facades\Broadcast;
 
-Broadcast::channel('channel_for_everyone', function ($user, $id) {
-    //return (int) $user->id === (int) $id;
+
+//Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
+//    return (int) $user->id === (int) $id;
+//});
+
+
+Broadcast::channel('channel_for_everyone', function ($user) {
     return true;
 });
